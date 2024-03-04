2e0266ab46ef27df1c404a831bc5e647c3052874
mecik111 <148684890+mecik111@users.noreply.github.com>
2024-03-04 19:52:19 +0100

    Add files via upload

---
+++ BattleShipGame/Player.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Player
+{
+    public Board Board { get; }
+    public Board EnemyBoard { get; }
+
+    public int playerWins = 0;
+   
+
+    public Player()
+    {
+        Board = new Board();
+    }
+
+    public void SetupBoard()
+    {
+        Board.ManualShipPlacement();
+    }
+
+    public void Attack(Player opponent)
+    {
+        opponent.Board.Display(false);
+        Console.WriteLine("Enter coordinates to attack:");
+        string input = Console.ReadLine().ToUpper();
+        int[] coords = Board.ConvertInputToCoordinates(input);
+        Console.Clear();
+        if(opponent.Board.grid[coords[0], coords[1]]=='O' || opponent.Board.grid[coords[0], coords[1]] == 'X')
+        {
+            Console.WriteLine("You have already shot at this place");
+            Attack(opponent);
+            return;
+        }
+        bool result = opponent.Board.ReceiveAttack(coords,opponent.Board);
+        if (result)
+        {
+            Console.WriteLine("Hit!");
+            if(opponent.AllShipsSunk())
+            {
+                return;
+            }
+            Attack(opponent);
+            return;
+        }
+            
+        else
+            Console.WriteLine("Miss!");
+
+        Console.WriteLine("\nOpponent's Board:");
+        opponent.Board.Display(false);
+    }
+
+    public bool AllShipsSunk()
+    {
+        return Board.AllShipsSunk();
+    }
+}
